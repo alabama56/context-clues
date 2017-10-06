df3f8d68154fd57886eea3f5552d015cde0a2c03
alabama56 <31999279+alabama56@users.noreply.github.com>
2017-10-05 20:33:18 -0500

    both index.jw and second.js work but second.js implements .bind in the proper way

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
 var friends = ["Bobby", "Billy", "Joey", "Frank", "Fred"];
 var city = ["Birmingham", "San Fansisco", "Fargo", "Tokyo", "Paris", "Dallas", "New York", "Boston", "Jacksonville", "Knoxville"];
 var weapons = ["Knife", "Pistol", "Rifle", "Fork", "Spork", "Bat", "Pencil", "Hammer", "Mallet", "Rocket", "Num-Chucks", "Fist", "Rock", "Sharpend Stick", "Shoestring", "Piano Wire", "Judy Chop", "Falcon Kick", "Golf Club", "Paperclip"];
-var buddyTotal = friends.length;
-var weaponTotal = weapons.length;
-var cityTotal = city.length;
-
 
 $(document).ready(function(){
     for(var i = 1; i <= 100; i++){
         var clue = $("<h3 id=" + i + ">Accusation " + i + "</h3>");
         $("body").append(clue);
 
-        
-
         $(clue).click(text.bind(null, random(friends), random(weapons), random(city)));
 
-        $(clue).click(function(){
-            
+        $(clue).click(function(){    
         });
     }  
-
-
-      
-    
+     
 });
 
 $('h3').bind('click', function() { alert("I  accuse " + random(friends) + ", with the " + random(weapons) + " in the city of " + random(city) )});
 
-function text(){
+function text(one,two,three){
     alert("I  accuse " + one + ", with the " + two + " in the city of " + three );   
 }
 
--- second.js
+++ second.js
@@ -1,28 +1,27 @@
 var friends = ["Bobby", "Billy", "Joey", "Frank", "Fred"];
 var city = ["Birmingham", "San Fansisco", "Fargo", "Tokyo", "Paris", "Dallas", "New York", "Boston", "Jacksonville", "Knoxville"];
 var weapons = ["Knife", "Pistol", "Rifle", "Fork", "Spork", "Bat", "Pencil", "Hammer", "Mallet", "Rocket", "Num-Chucks", "Fist", "Rock", "Sharpend Stick", "Shoestring", "Piano Wire", "Judy Chop", "Falcon Kick", "Golf Club", "Paperclip"];
-var buddyTotal = friends.length;
-var weaponTotal = weapons.length;
-var cityTotal = city.length;
-
 
 $(document).ready(function(){
     for(var i = 1; i <= 100; i++){
         var clue = $("<h3 id=" + i + ">Accusation " + i + "</h3>");
         $("body").append(clue);
 
-        $(clue).click(text.bind(null, random(friends), random(weapons), random(city)));
+        var master = {
+            friend: random(friends),
+            weapon: random(weapons),
+            town: random(city)
+        };
 
-        $(clue).click(function(){
-            
+        $(clue).click(text.bind(master));
+
+        $(clue).click(function(){  
         });
     }     
 });
 
-$('h3').bind('click', function() { alert("I  accuse " + random(friends) + ", with the " + random(weapons) + " in the city of " + random(city) )});
-
 function text(){
-    alert("I  accuse " + one + ", with the " + two + " in the city of " + three );   
+    alert("I  accuse " + this.friend + ", with the " + this.weapon + " in the city of " + this.town );   
 }
 
 function random(array) {
